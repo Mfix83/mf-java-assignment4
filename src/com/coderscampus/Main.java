a7b0bc3b92c24455d0d725d432c09ee559dccccf
Mfix83 <123844738+Mfix83@users.noreply.github.com>
2023-09-09 16:58:53 -0400

    aa

--- src/com/coderscampus/Main.java
+++ src/com/coderscampus/Main.java
@@ -1,25 +1,21 @@
 package com.coderscampus;
 
-import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;   
+import java.util.Comparator;
 import java.util.List;
-
+ 
 public class Main {
     public static void main(String[] args) {
         String csvFile = "data/StudentList.csv";
         List<String[]> studentDataList = new ArrayList<>();
-        List<String[]> course1Students = new ArrayList<>();
-        List<String[]> course2Students = new ArrayList<>();
-        List<String[]> course3Students = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             String line;
-            br.readLine();
+            br.readLine(); // Skip the header line
             while ((line = br.readLine()) != null) {
                 String[] studentData = line.split(",");
                 studentDataList.add(studentData);
@@ -28,36 +24,13 @@ public class Main {
             e.printStackTrace();
         }
 
+        // Sort all students by grade
+        Collections.sort(studentDataList, new GradeComparator());
+
+        // Display all student names
         for (String[] studentData : studentDataList) {
-            String course = studentData[2].trim(); // Trim the course code
-
-            if (course.equals("COMPSCI 310") || course.equals("COMPSCI 311") || course.equals("COMPSCI 312")) {
-                course1Students.add(studentData);
-            } else if (course.equals("APMTH 129") || course.equals("APMTH 130") || course.equals("APMTH 131")) {
-                course2Students.add(studentData);
-            } else if (course.equals("STAT 234") || course.equals("STAT 235") || course.equals("STAT 236")) {
-                course3Students.add(studentData);
-            }
-        }
-
-        Collections.sort(course1Students, new GradeComparator());
-        Collections.sort(course2Students, new GradeComparator());
-        Collections.sort(course3Students, new GradeComparator());
-
-        writeStudentsToCSV(course1Students, "course1.csv");
-        writeStudentsToCSV(course2Students, "course2.csv");
-        writeStudentsToCSV(course3Students, "course3.csv");
-    }
-
-    private static void writeStudentsToCSV(List<String[]> students, String filename) {
-        try (FileWriter writer = new FileWriter(filename)) {
-            writer.write("Student ID,Student Name,Course,Grade\n");
-            for (String[] studentData : students) {
-                String studentInfo = String.join(",", studentData);
-                writer.write(studentInfo + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            String studentName = studentData[1].trim(); // Assuming student name is in the second column
+            System.out.println(studentName);
         }
     }
 
